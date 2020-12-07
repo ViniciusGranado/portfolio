35bda45bb3b21ce4058e4ac6c5d21eb95f54d724
Vinicius Granado <vinicius.r.granado@gmail.com>
2020-12-07 20:46:50 -0300

    Update header-animation js

--- script/header-animation.js
+++ script/header-animation.js
@@ -23,51 +23,11 @@ sections.forEach(section => {
 function animateHeader (elementId) {
   const menuItens = document.querySelectorAll('.menu-link');
 
-  switch (elementId) {
-    case 'intro':
-      menuItens.forEach((item) => {
-        if (item.classList.contains('menu-intro')) {
-          item.classList.add('active');
-        } else {
-          item.classList.remove('active');
-        }
-      });
-      break;
-
-    case 'portfolio':
-      menuItens.forEach((item) => {
-        if (item.classList.contains('menu-portfolio')) {
-          console.log('portfolio');
-          item.classList.add('active');
-        } else {
-          item.classList.remove('active');
-        }
-      });
-      break;
-
-    case 'about':
-      menuItens.forEach((item) => {
-        if (item.classList.contains('menu-about')) {
-          console.log('about');
-          item.classList.add('active');
-        } else {
-          item.classList.remove('active');
-        }
-      });
-      break;
-
-    case 'contact':
-      menuItens.forEach((item) => {
-        if (item.classList.contains('menu-contact')) {
-          console.log('contact');
-          item.classList.add('active');
-        } else {
-          item.classList.remove('active');
-        }
-      });
-      break;
-
-    default:
-      return;
-  }
+  menuItens.forEach((item) => {
+    if (item.getAttribute('data-link') === elementId) {
+      item.classList.add('active');
+    } else {
+      item.classList.remove('active');
+    }
+  })
 }
